fd052ce98f5589c3ed51a0646309ebc4aba98d20
ramzimah <ramzimahnoune@gmail.com>
2020-11-27 22:23:09 +0100

    started working on the profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 //@route get api/profile/me
 //@desc get current users profile
 //@access private
-router.get("/", (req, res) => {
+router.get("/", auth, (req, res) => {
   res.send("profile route");
 });
 
