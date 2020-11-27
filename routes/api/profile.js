cae75a10661fc9ed52d3b8a97c4fb6179d3f86d1
ramzimah <ramzimahnoune@gmail.com>
2020-11-27 22:06:37 +0100

    fixed some issues on gitignore file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-//@route get api/profile
-//@desc test route
-//@access public
+//@route get api/profile/me
+//@desc get current users profile
+//@access private
 router.get("/", (req, res) => {
   res.send("profile route");
 });
