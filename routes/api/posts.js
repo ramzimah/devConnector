5390b36599d247a86fffe4e57322d4c31b41f6c1
ramzimah <ramzimahnoune@gmail.com>
2020-12-01 16:16:39 +0100

    added  add and delete comments functionalities

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -19,9 +19,7 @@ router.post(
     }
 
     try {
-      const user = await (await User.findOneById(req.user.id)).select(
-        "-password"
-      );
+      const user = await (await User.findById(req.user.id)).select("-password");
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
@@ -133,6 +131,66 @@ router.put("/unlike/:id", auth, async (req, res) => {
     post.likes.splice(removeIndex, 1);
     await post.save();
     res.json(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    res.send(500).send("server error");
+  }
+});
+//@route post api/comments/:id
+//@desc comment on a post
+//@access private
+router.post(
+  "/comments/:id",
+  [auth, check("text", "text is required").notEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.this.status(400).json({ errors: errors.array });
+    }
+
+    try {
+      const user = await (await User.findById(req.user.id)).select("-password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.send(500).send("server error");
+    }
+  }
+);
+
+//@route delete api/comments/:id/:comment_id
+//@desc  delete a comment
+//@access private
+router.delete("/comments/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //pull out comment
+    const comment = await post.comment.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    //check if the comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "comment does not exist" });
+    }
+    //check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "user not authorized" });
+    }
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
   } catch (err) {
     console.log(err.message);
     res.send(500).send("server error");
