c79ba7a12137bdd4e768e083a1480980b60335bc
ramzimah <ramzimahnoune@gmail.com>
2020-12-02 11:59:50 +0100

    created register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import react, { Fragment } from "react";
-
-const App = () => {
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>;
-};
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+function App() {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  );
+}
 
 export default App;
