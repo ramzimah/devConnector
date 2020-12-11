fffeb7b671521ca1c979351892f422d82b8c52a5
ramzimah <ramzimahnoune@gmail.com>
2020-12-12 00:39:53 +0100

    created the login component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -16,15 +17,7 @@ function Register() {
     if (password !== password2) {
       console.log("passwords do not match");
     } else {
-      console.log(formData);
-      const newUser = {
-        name,
-        email,
-        password,
-        password2,
-      };
-      try {
-      } catch (err) {}
+      console.log("sucsess");
     }
   };
   return (
@@ -80,7 +73,7 @@ function Register() {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
